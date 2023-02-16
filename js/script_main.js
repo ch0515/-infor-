b0c53430e7f597a2167bf9c620cfe2d8597b5a23
ch0515 <w2114@e-mirim.hs.kr>
2023-02-17 01:01:33 +0900

    intro_dining 영역_02 완성

--- js/script_main.js
+++ js/script_main.js
@@ -15,6 +15,17 @@ $(function(){
         pauseOnHover: false,//마우스 호버시 정지
         pauseOnFocus: false // 포커스시 정지
     });
+
+    //slide2
+    $('.intro_dining .slide2').slick({
+        arrows: false,   //화살표
+        dots: false, // 인디케이스 해제
+        fade: false, //페이드 효과
+        autoplay: true, //자동재생
+        autoplaySpeed: 4000,//재생시간
+        pauseOnHover: false,//마우스 호버시 정지
+        pauseOnFocus: false // 포커스시 정지
+    });
 });
 
 //탭메뉴
